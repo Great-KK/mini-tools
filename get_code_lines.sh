da388333f8ba859e0bbaaa5e8d170ce9eebb50ea
Xherlock <40600465+Great-KK@users.noreply.github.com>
2020-10-21 12:13:39 +0800

    Update get_code_lines.sh
    set  tab=4

--- get_code_lines.sh
+++ get_code_lines.sh
@@ -5,26 +5,25 @@ echo "count code lines"
 
 # To count code lines iterative
 iterFunc() {
-	line_num=0
-	if [[ -f $1 ]]
-	then
-		line_num=`wc -l $1 | awk -F' ' '{print $1}'`
-		line_num=$[$line_num+1]
-	elif [[ -d $1 ]]
-	then
-		cd $1
-		rm -rf tmp_for_count_code_lines
+    line_num=0
+    if [[ -f $1 ]]
+    then
+    	line_num=`wc -l $1 | awk -F' ' '{print $1}'`
+	line_num=$[$line_num+1]
+    elif [[ -d $1 ]]
+    then
+        cd $1
         ls > ./tmp_for_count_code_lines
-		# delete tmp_for_count_code_lines in tmp_for_count_code_lines
-		sed -i '$d' ./tmp_for_count_code_lines
+	# delete tmp_for_count_code_lines in tmp_for_count_code_lines
+	sed -i '$d' ./tmp_for_count_code_lines
         while read line
-		do
-			tmp_num=`iterFunc $line`
-			line_num=$[$line_num+$tmp_num]
-		done < tmp_for_count_code_lines
+	do
+	    tmp_num=`iterFunc $line`
+	    line_num=$[$line_num+$tmp_num]
+	done < tmp_for_count_code_lines
         rm -rf ./tmp_for_count_code_lines
-	fi
-	echo $line_num
+    fi
+    echo $line_num
 }
 
 total_line_num=`iterFunc ./`
